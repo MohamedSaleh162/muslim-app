733feec2dc1c14d12ca131662374ea826fef697b
Mohamed Saleh <thm64227@gmail.com>
2026-09-17 21:23:00 +0300

    Change SW fetch strategy to Network First

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "muslim-app-v12";
+const CACHE_NAME = "muslim-app-v13";
 const assets = [
   "./",
   "./index.html",
@@ -13,19 +13,15 @@ const assets = [
   "./Images/Logo-removebg-preview (Edited).png"
 ];
 
-// 1. File storage and skip the wait
 self.addEventListener("install", installEvent => {
   self.skipWaiting();
-  
   installEvent.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
-      console.log("Files successfully cached.");
       return cache.addAll(assets);
     })
   );
 });
 
-// 2. Activate the update and clear any old cache (this is the step you were missing).
 self.addEventListener("activate", activateEvent => {
   activateEvent.waitUntil(
     caches.keys().then(keys => {
@@ -34,13 +30,21 @@ self.addEventListener("activate", activateEvent => {
       );
     })
   );
+  self.clients.claim();
 });
 
-// 3. Retrieving files (with or without an internet connection)
 self.addEventListener("fetch", fetchEvent => {
   fetchEvent.respondWith(
-    caches.match(fetchEvent.request).then(res => {
-      return res || fetch(fetchEvent.request);
-    })
+    fetch(fetchEvent.request)
+      .then(res => {
+        const resClone = res.clone();
+        caches.open(CACHE_NAME).then(cache => {
+          cache.put(fetchEvent.request, resClone);
+        });
+        return res;
+      })
+      .catch(() => {
+        return caches.match(fetchEvent.request);
+      })
   );
 });
